edf6fafec069ac0e1de035acaa322494c611e290
Hasanga <sajana@Hasangas-MacBook-Air.local>
2026-02-09 09:51:17 +0530

    Initial commit - portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: "/Portfolio"
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
 })
